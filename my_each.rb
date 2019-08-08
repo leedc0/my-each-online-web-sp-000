8870b7e453c6293bbcd22322b1150905bb418e42
Daniel Lee <leedc@me.com>
2019-08-08 03:27:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
 def my_each(array) # put argument(s) here
   # code here
   words = 0
-  while words < array.length
-    yield array[words]
+  while words < array.length 
+    yield array(words)
     words += 1
   end
-  array
 end
